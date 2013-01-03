8363c713272052c564c37b8adaa260aea120e017
Kiddikn <kiddikn@gmail.com>
2013-01-03 21:03:35 +0900

    done commit

--- 3/kadai3.c
+++ 3/kadai3.c
@@ -1,15 +1,16 @@
 /* kadai3.c 2Dの動画プログラム */
 #include <stdlib.h>
 #include <GLUT/glut.h> 
+#include <math.h>
 
 #define   rocket      1  //ロケット 
 #define   star        2  //星
-
+#define r2 2
+#define r1 1
 float rote = 0.;  
 float speed = 0.001; // 図形の移動速度 
 float accel = 0.001; // 移動速度を調整するパラメータ 
 unsigned char moveFlag = GL_TRUE;// 星の運動(1)か(0)かのフラグ 
-int xBegin, yBegin, mButton;
 float distance;
 
 void resetview(void);
@@ -22,12 +23,15 @@ void myinit(void)
     float engine[][2]={-0.3,-5.4,0.3,-5.4,0.,-4.8};
     float leftwing[][2]={-1.,-3.0,-2.,-4.2,-2.,-5.5,-1.,-5.};
     float rightwing[][2]={1.,-3.0,1.,-5.,2.,-5.5,2.,-4.2};
+    float Ax,Ay,Bx,By,Cx,Cy;
+    float tx = 0.0,ty=0.0;
 
     glClearColor(0.,0.,0.,1.);    // 消去色 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 消去 
 
     glNewList( rocket, GL_COMPILE );  // ロケットの表示リスト作成    
         glPushMatrix();
+        glRotatef(45.0,1.0,1.0,0.0);
         glTranslatef(0.0, 1.0, -10.0);
         glBegin(GL_QUADS);
         for( i=0; i<4; i++ )glVertex2fv(quads[i]); //本体     
@@ -47,24 +51,61 @@ void myinit(void)
         glPopMatrix();
    glEndList(); 
 
-//    glNewList( star, GL_COMPILE);     //星の作成
-
-  //  glEndList();
+   glNewList( star, GL_COMPILE);     //星の作成
+       glPushMatrix();
+       glTranslatef(0.0, 1.0, -10.0);
+       glScalef(0.1,0.1,0.1);  
+       glBegin(GL_TRIANGLE_STRIP);
+       for (i=0; i<5; i++){
+           Ax = tx + r2*sin(2*M_PI*2*i/10);
+           Ay = ty + r2*cos(2*M_PI*2*i/10);
+           Bx = tx + r1*sin(2*M_PI*(2*i+1)/10);
+           By = ty + r1*cos(2*M_PI*(2*i+1)/10);
+           Cx = tx + r2*sin(2*M_PI*(2*i+2)/10);
+           Cy = ty + r2*cos(2*M_PI*(2*i+2)/10);
+           glVertex2d(tx , ty);
+           glVertex2f(Ax, Ay);
+           glVertex2f(Bx, By);
+           glVertex2d(tx , ty);
+           glVertex2f(Bx, By);
+           glVertex2f(Cx, Cy);
+       }
+       glEnd();
+       glPopMatrix();
+   glEndList();
 } 
 
 void display(void) 
-{ 
+{   
+    int i;
+    float xDisp[]={1.0,3.0,5.0,6.0,7.0};
+    float yDisp[]={2.0,8.0,5.0,0.3,4.4};
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    glTranslatef(0.0, 0.0, -distance);    
+    glColor3f(0.0,0.0,0.0);
+    glTranslatef(3.0, 0.0, -distance);    
     if( moveFlag ) rote += speed; 
     glEnable(GL_DEPTH_TEST); //物体がかぶらないように計算    
+    
+    glPushMatrix();
     glRotatef(rote,1.0,0.0,0.0);
     glColor3f( 1.,0.,0. ); 
     glCallList(rocket); 
+    glPopMatrix();
+    glColor3f(1.0,1.0,1.0);
+    glCallList(star);          
+    glPushMatrix();
+    glTranslatef(-6.0,-6.0,5.0);
+    glCallList(star);
+    glPopMatrix();   
 
-   // glTranslatef( 0.,rote,0. );   // (0,0,1)軸の周りにrote度の回転 
-    glDisable(GL_DEPTH_TEST);
+    glPushMatrix();
+    glTranslatef(-6.0,6.0,0.0);
+    glCallList(star);
+    glPopMatrix();   
+
+   glDisable(GL_DEPTH_TEST);
     glutSwapBuffers(); 
 } 
 
@@ -96,7 +137,7 @@ void key_input(unsigned char key, int x, int y) //キー振り分け登録
                   break;
         case ' ': moveFlag = !moveFlag; 
                   if( moveFlag ) glutIdleFunc(display); 
-                  else  glutIdleFunc(0);
+                  else  glutIdleFunc(0);   //動かさない
         default:   break; 
     } 
 } 
@@ -104,8 +145,6 @@ void key_input(unsigned char key, int x, int y) //キー振り分け登録
 void special_input(int key, int x, int y) //特別キー 
 { 
     switch(key){ 
-        case GLUT_KEY_LEFT: 
-        case GLUT_KEY_RIGHT:break; 
         case GLUT_KEY_UP:   speed += accel; moveFlag=GL_TRUE; 
                             glutIdleFunc(display); break; 
         case GLUT_KEY_DOWN: speed -= accel; moveFlag=GL_TRUE; 
@@ -113,44 +152,6 @@ void special_input(int key, int x, int y) //特別キー
         default: return; 
     } 
 } 
-
-void myMouse( int button, int state, int x, int y )
-{
-    if (state == GLUT_DOWN) {
-        switch(button) {
-            case GLUT_LEFT_BUTTON:
-                mButton = button;
-                break;
-            case GLUT_RIGHT_BUTTON:
-                mButton = button;
-                break;
-        }
-        xBegin = x;
-        yBegin = y;
-    }
-}
-
-void myMotion( int x, int y )
-{
-    int xDisp, yDisp;
-
-    xDisp = x - xBegin;    //差分をとる
-    yDisp = y - yBegin;
-
-    switch (mButton) {
-       /* case GLUT_LEFT_BUTTON:
-            azimuth += (float) xDisp/2.0;
-            elevation -= (float) yDisp/2.0;
-            break;*/
-        case GLUT_RIGHT_BUTTON:
-            distance=(distance == 10)? 10:(distance+ (float) yDisp/40.0);
-            break;
-    }
-    xBegin = x;
-    yBegin = y;
-    glutPostRedisplay();
-}
-
 
 int main(int argc, char **argv) 
 { 
@@ -160,14 +161,12 @@ int main(int argc, char **argv)
     glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH); 
     glutCreateWindow(argv[0]); 
     myinit(); 
+    glutKeyboardFunc(key_input);  // 通常キー入力による割込関数 
+    glutSpecialFunc(special_input); //　矢印キー入力で呼ばれる関数を指定 
+    resetview();
     glutDisplayFunc(display); // 描画用関数を指定 
     glutReshapeFunc(reshape); // 再描画時に呼ばれる関数を指定 
     glutIdleFunc(display); // 常時呼ばれる関数を指定 
-    glutKeyboardFunc(key_input);  // 通常キー入力による割込関数 
-    glutSpecialFunc(special_input); //　矢印キー入力で呼ばれる関数を指定 
-    glutMouseFunc(myMouse);         //マウスに反応
-    glutMotionFunc(myMotion);      //拡大縮小に対応
-    resetview();
     glutMainLoop(); 
     return 0; 
 } 
